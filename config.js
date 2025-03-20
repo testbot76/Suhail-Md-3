c86009240dd388818be8e34c3f15b977116a2ba4
testbot76 <testbot.coding76@gmail.com>
2025-03-21 04:09:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md-
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07";
 global.website =process.env.GURL || "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.ibb.co/TgwRyHR/Ephoto360-com-166de101e03cd7.jpg" || "https://i.ibb.co/bH1kbX0/a4c0b1af253197d4837ff6760d5b81c0.jpg" ; // SET LOGO FOR IMAGE 
-global.caption = process.env.CAPTION || global.caption || "MUDASIR SPORTS 76" 
+global.caption = process.env.CAPTION || global.caption || "> MUDASIR SPORTS 76" 
 
 
 global.devs = "" // Developer Contacts
